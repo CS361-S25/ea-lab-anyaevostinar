94253f626502bfd8eebc33a8ac883323030e4534
Anya Vostinar <vostinar@carleton.edu>
2025-04-04 09:22:28 -0500

    starting on my ea, yey

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include "Empirical/include/emp/math/Random.hpp"
 
+//Organism class with instance variable double behavior
+class Organism {
+public:
+    // Instance variable
+    double behavior;
+
+    // Constructor to initialize behavior
+    Organism(double b) : behavior(b) { }
+
+    // Function to get the behavior
+    double GetBehavior() const {
+        return behavior;
+    }
+
+    // Function to set the behavior
+    void SetBehavior(double b) {
+        behavior = b;
+    }
+};
+
+
+
 int main() {
 
 }
